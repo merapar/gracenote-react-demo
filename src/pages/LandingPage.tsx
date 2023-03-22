e69e69cbc55a44b942905b22839367d3a32f526e
Ario Noroozpoor <ario.noroozpoor@merapar.com>
2023-03-22 12:17:07 +0000

    add landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,5 +1,11 @@
 import styles from "./LandingPage.module.css";
 
 export const LandingPage = () => {
-  return <div className={styles.content}>Landing Page</div>;
+  return (
+    <div className={styles.content}>
+      <h3>This is the Landing page.</h3>
+      <p>Depending on the design, perhaps router to be added</p>
+      <p>...</p>
+    </div>
+  );
 };
