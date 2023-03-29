e9989838f13e0e911411d3885a956b1a698d51cd
Marek Zielonkowski <marek.zielonkowski@merapar.com>
2023-03-29 09:25:31 +0200

    api transport layer for showings and theaters

--- src/api/http/url.ts
+++ src/api/http/url.ts
@@ -1,1 +1,1 @@
-export const API_URL = `http://data.tmsapi.com/v1.1`; //APP_CONFIG.API_URL;
+export const API_URL = `http://data.tmsapi.com/v1.1`;
--- src/config.d.ts
+++
@@ -1,5 +0,0 @@
-declare var APP_CONFIG: {
-    VERSION: string;
-    API_URL: string;
-    APP_URL: string;
-}
