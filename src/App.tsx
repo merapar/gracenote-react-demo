f21b2c16c3d6d66fd27e7a2eda8cfeb4f760d1bc
ComaQuasar <ComaQuasar@users.noreply.github.com>
2023-03-31 10:37:23 +0000

    deploy: 3b424e1f5a4872ba623c1204037f43cf813bd889

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import { LandingPage } from "./pages/LandingPage";
-
-function App() {
-  return <LandingPage />;
-}
-
-export default App;
--- src/api/http/errors/ServerError.ts
+++
@@ -1,9 +0,0 @@
-import { ErrorResponseParams, RequestError } from "./RequestError";
-
-export class ServerError extends RequestError {
-  constructor(params: ErrorResponseParams) {
-    super(params);
-
-    this.name = "ServerError";
-  }
-}
