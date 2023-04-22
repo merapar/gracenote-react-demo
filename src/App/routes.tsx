af091651e452b9b1a1d2cdbc2ba697bd78f7e56e
Jaap Haitsma <jaap.haitsma@merapar.com>
2023-04-22 18:08:45 -0400

    Change back to Browser router

--- src/App/routes.tsx
+++ src/App/routes.tsx
@@ -1,4 +1,4 @@
-import { createHashRouter } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 
 import App from '../App';
 import { ErrorPage } from '../pages/ErrorPage';
@@ -23,36 +23,38 @@ export const getRouteTitleByPath = (path: string) => {
   return routesConfig.find((value) => value.url === path)?.title;
 };
 
-export const routes = createHashRouter([
-  {
-    element: <App />,
-    errorElement: <ErrorPage />,
-
-    children: [
-      {
-        path: URL_LANDING_PAGE,
-        element: (
-          <RequireApiKey>
-            <MoviesOnCinema />
-          </RequireApiKey>
-        ),
-      },
-      {
-        path: URL_WHATS_ON_TV,
-        element: (
-          <RequireApiKey>
-            <MoviesOnTv />
-          </RequireApiKey>
-        ),
-      },
-      {
-        path: URL_ABOUT,
-        element: <AboutPage />,
-      },
-      {
-        path: URL_API_KEY_PAGE,
-        element: <APIKeyPage />,
-      },
-    ],
-  },
-]);
+export const routes = createBrowserRouter(
+  [
+    {
+      element: <App />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: URL_LANDING_PAGE,
+          element: (
+            <RequireApiKey>
+              <MoviesOnCinema />
+            </RequireApiKey>
+          ),
+        },
+        {
+          path: URL_WHATS_ON_TV,
+          element: (
+            <RequireApiKey>
+              <MoviesOnTv />
+            </RequireApiKey>
+          ),
+        },
+        {
+          path: URL_ABOUT,
+          element: <AboutPage />,
+        },
+        {
+          path: URL_API_KEY_PAGE,
+          element: <APIKeyPage />,
+        },
+      ],
+    },
+  ],
+  { basename: process.env.PUBLIC_URL },
+);
