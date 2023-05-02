246727a1de7aad3adbeea35cd244777602eaadee
Jaap Haitsma <jaap.haitsma@merapar.com>
2023-05-02 12:35:22 -0400

    Better error message when there is no data

--- src/components/ContentDashboard.tsx
+++ src/components/ContentDashboard.tsx
@@ -1,4 +1,4 @@
-import { LinearProgress } from '@mui/material';
+import { Alert, LinearProgress, Snackbar } from '@mui/material';
 
 import { ContentGallery, ContentItem } from './ContentGallery';
 
@@ -12,12 +12,14 @@ export const ContentDashboard = ({
   return (
     <main>
       {isLoading && <LinearProgress />}
-      {!isLoading && !contentItems && (
-        <div>No data available for this date and location</div>
-      )}
-      {!isLoading && contentItems && (
+      {!isLoading && contentItems?.length && (
         <ContentGallery contentItems={contentItems} />
       )}
+      <Snackbar open={!isLoading && !contentItems?.length}>
+        <Alert sx={{ width: '100%' }} severity="error">
+          No data found for this date and location
+        </Alert>
+      </Snackbar>
     </main>
   );
 };
