111a0c5300ea52f4992d604cb6a9454539bf6625
Jaap Haitsma <jaap.haitsma@merapar.com>
2023-05-02 12:42:48 -0400

    Replace constant

--- src/components/WithParamSelector.tsx
+++ src/components/WithParamSelector.tsx
@@ -2,10 +2,11 @@ import { Outlet } from 'react-router-dom';
 import { ParamSelector } from './ParamSelector';
 import dayjs, { Dayjs } from 'dayjs';
 import { useState } from 'react';
+import { Locations } from './LocationSelector';
 
 export default function WithParamSelector() {
   const [selectedDate, setSelectedDate] = useState<Dayjs | null>(dayjs());
-  const [zipCode, setZipCode] = useState<number>(11554);
+  const [zipCode, setZipCode] = useState<number>(Locations['New York']);
 
   return (
     <div>
